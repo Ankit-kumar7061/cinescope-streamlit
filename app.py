327b49c480a1683be0282c6d8782c8242e972323
Ankit-kumar7061 <ankitkr99018@gmail.com>
2025-08-06 00:21:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,9 @@ st.dataframe(filtered_df[['Title', 'Popularity', 'Vote_Count']], use_container_w
 st.markdown("---")
 st.markdown("### 🍿 Top 5 Popular Movies (Based on Filter)")
 
+st.markdown("---")
+st.caption("© 2025 CineScope | 👨‍💻 Developed by **Ankit Kumar**")
+
 top5 = filtered_df[['Title', 'Popularity']].sort_values(by='Popularity', ascending=False).head(5)
 
 if not top5.empty:
@@ -64,4 +67,5 @@ if not top5.empty:
     st.pyplot(fig)
 else:
     st.warning("No data available for the selected filters.")
+
 
